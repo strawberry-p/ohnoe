72b3329d6eb685faaf9bce77e97d8b03718777b6
strawberry-p <strawberry@example.com>
2026-01-06 11:44:53 +0100

    bot responds with blockkit to get the data

--- bot.py
+++ bot.py
@@ -1,16 +1,100 @@
 import os
-import dotenv
+import dotenv, json
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 dotenv.load_dotenv()
 
 # Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"),signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
+print(os.environ.get("SLACK_BOT_TOKEN")[:10]) #type: ignore
 
-@app.message("lock in")
+
+@app.message("lockin")
 def message_lockin(message,say):
-    say(f"lock the hell in twin")
+    print("received "+str(message))
+    blocks = json.loads("""{
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "that is true. elaborate.",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "When's the deadline?"
+			},
+			"accessory": {
+				"type": "datepicker",
+				"initial_date": "2026-01-07",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select a date",
+					"emoji": true
+				},
+				"action_id": "datepicker-action"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "What time?"
+			},
+			"accessory": {
+				"type": "timepicker",
+				"initial_time": "23:59",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select time",
+					"emoji": true
+				},
+				"action_id": "timepicker-action"
+			}
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "task_label_input-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Name",
+				"emoji": true
+			},
+			"optional": false
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Add task",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"action_id": "submit_button-action"
+				}
+			]
+		}
+	]
+}""")["blocks"]
+    print(say(blocks=blocks,text="Add a task. You will not regret it."))
 
-# Start your app
+@app.action("submit_button-action")
+def action_submit(ack, body, logger):
+    ack()
+    logger.info(body)
+
+
 if __name__ == "__main__":
+    print("why it no workie :(")
+    #app.start(port=int(os.environ.get("PORT", 3000)))
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
