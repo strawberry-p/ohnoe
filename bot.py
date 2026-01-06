ef75df00caab95dd089dbf17facd607662caf946
strawberry-p <strawberry@example.com>
2026-01-06 15:25:30 +0100

    tasks can now be sent from the slack response message and are saved

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import datetime as dt
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 dotenv.load_dotenv()
+DATA_FILE = "bot-data.json"
+lastSubmitName = ""
 try:
     with open("bot-data.json") as file:
         stored = json.load(file)
@@ -27,7 +29,22 @@ class Scheduled:
         self.text = text
     def until(self):
         return self.ts-dt.datetime.now().timestamp
+    def __dict__(self):
+        return {"name":self.name,"id": self.id, "ts": self.ts,"userID":self.userID,"created":self.created,"iter":self.iter,"text":self.text}
         
+
+def scheduled(name,ts,created,userID,j=0,text=""):
+    return {"name":name,"id": userID+"_"+name+"_"+str(created), "ts": ts,"userID":userID,"created":created,"iter":j,"text":text}
+
+def get_submitted_data(inp: dict):
+    global nextDate,nextTime,nextLabel
+    if inp.get("datepicker-action","-1") != "-1":
+        nextDate = inp["datepicker-action"]["selected_date"]
+    if inp.get("timepicker-action","-1") != "-1":
+        nextTime = inp["timepicker-action"]["selected_time"]
+    if inp.get("task_label_input-action",-1) != -1: #avoiding intended string
+        nextLabel = inp["task_label_input-action"]["value"]
+
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"),signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
 print(os.environ.get("SLACK_BOT_TOKEN")[:10]) #type: ignore
@@ -119,8 +136,41 @@ def message_alt_lock_in(message,say):
 
 @app.action("submit_button-action")
 def action_submit(ack, body, logger):
+    global nextDate,nextTime,nextLabel,lastSubmitName,stored,data
     ack()
     print("submit "+str(body["state"]["values"]))
+    print(body.get('value',"no value in submit button"))
+    for k in body["state"]["values"]:
+        v = body["state"]["values"][k]
+        get_submitted_data(v)
+    
+    print((nextDate,nextTime,nextLabel))
+    splitDate = [int(x) for x in nextDate.split("-")]
+    splitTime = [int(x) for x in nextTime.split(":")]
+    obj = scheduled(nextLabel,dt.datetime(splitDate[0],splitDate[1],splitDate[2],splitTime[0],splitTime[1]).timestamp(), #type:ignore
+                    dt.datetime.now().timestamp(),body["user"]["id"],text="Hello. My name is Jigsaw.")
+    print(f"body length {len(str(body))}")
+    print(body["trigger_id"])
+    if nextLabel == lastSubmitName:
+        print('deduped')
+    else:
+        data.append(obj)
+        stored["data"].append(obj)
+        if not (body["user"]["id"] in stored["users"]):
+            stored["users"].append(body["user"]["id"])
+        lastSubmitName = nextLabel
+    with open(DATA_FILE,"w") as file:
+            try:
+                print(f"stored {stored}")
+                print(f"data {data}")
+                json.dump(stored,file)
+            except Exception as _:
+                print(_)
+                print(stored)
+                print(data)
+    nextDate,nextTime,nextLabel = (0,0,0)
+    splitDate = []
+    splitTime = []
     logger.info(body)
 
 @app.action("datepicker-action")
@@ -141,4 +191,15 @@ def action_tasklabel(ack,body,logger):
 
 if __name__ == "__main__":
     #app.start(port=int(os.environ.get("PORT", 3000)))
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    try:
+        SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    except KeyboardInterrupt:
+        print("interrupted")
+        print(data)
+        with open(DATA_FILE,"w") as file:
+            json.dump(stored,file)
+    except Exception as _:
+        print(f"exiting, {_}")
+        print(data)
+        with open(DATA_FILE,"w") as file:
+            json.dump(stored,file)
