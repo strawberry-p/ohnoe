7e81557f001caea433bd0673225dbd81ee21982a
DevMa7e1 <thedevma7e1@gmail.com>
2026-01-07 20:05:58 +0100

    Fix number 2 :3 oopsie

--- x_integration.py
+++ x_integration.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 app = Flask(__name__)
 def runApp():
     app.run("0.0.0.0", 8080)
-flaskThread = threading.Thread(target=runApp)
+threading.Thread(target=runApp).start()
 
 f = open("app_auth")
 r = f.read().split("\n")
